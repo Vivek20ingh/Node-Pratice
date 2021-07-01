129b582e1b0978462cf6fe14d13934992e60330c
Vivek Singh <vs2357766@gmail.com>
2021-07-01 12:08:12 +0530

    read and write file

---
+++ app5.js
@@ -0,0 +1,32 @@
+var fs=require('fs')
+
+// var read =fs.readFileSync('readme.txt', 'utf8');
+// console.log(read);
+
+
+// fs.writeFileSync('writeMe.txt',read);
+
+
+// fs.readFile('readme.txt', 'utf8', function(err,data)
+// {
+//     console.log(data);
+// });
+// fs.readFile('readme.txt', 'utf8', function(err,data)
+// {
+//     fs.writeFile('writeMe.txt',data,(err) =>
+//     {
+
+//     });
+// });
+
+// console.log('test');
+
+fs.unlink('writeme.txt',()=>
+{
+
+});
+// fs.mkdirSync('stuff');
+// fs.rmdirSync('stuff');
+fs.mkdir('stff',() =>{
+    
+})
